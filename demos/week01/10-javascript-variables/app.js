b7ca849cba3d43a0bf0c5fe0d0dd9ab8d5e7106a
Sam Louloudis <samanthaleelouloudis@gmail.com>
2025-01-08 16:23:05 +0000

    variables

---
+++ demos/week01/10-javascript-variables/app.js
@@ -0,0 +1,99 @@
+console.log('Hello world!')
+
+
+// I've created a 'variable' called myName that stores the value 'Sam'
+
+let myName = 'Sam'
+
+// It means I can use myName to refer to whatever value is stored insideit
+
+console.log(myName)
+
+
+// Datatypes
+
+// Strings
+let string1 = "Sam"
+let string2 = 'This is a string :) Whoop! **** dfw3r12034182309812'
+let myFavNumber = '42'
+
+// Numbers
+
+let number1 = 1
+let number2 = 34212
+let number3 = 1.2
+let year = 2025
+
+
+// Boolean
+let bestClassEver = true
+let frankieBestTA = false
+
+
+let userLoggedIn = true
+
+
+let a = 1
+let b = 2
+let c = 3
+
+let sum = a + b 
+console.log(sum) // 3
+
+let sub = c - a // 2
+let division = c / b // 1.5
+let product = c * b // 6
+
+let isLessThan = b < c // true 
+let isGreaterThan = b > c // false 
+
+
+console.log(isLessThan)
+
+
+// concatination 
+
+let taOne = 'Frankie'
+let taTwo = 'Angelica'
+
+const concatination = taOne + " " +taTwo
+
+console.log(concatination)
+
+// template literalts
+
+let templateLiterals = `Hi my name is sam, and I have two TA's - one is ${taOne} and the other is ${taTwo}. They are great!`
+
+
+// typeof is useful for finding out the datatype of a variable
+let coolerName = 42
+console.log(typeof coolerName)
+
+
+
+
+let myDog = 'Darcy'
+console.log(myDog) // Darcy
+myDog = 'Charlie'  // once I've created the varible, I don't need to use let again to create it.
+console.log(myDog) // Charlie
+
+
+const myHome = 'Earth'
+console.log(myHome)
+myHome = 'Mars' // We get an error - we cannot re-assign the value of constant varibles
+console.log(myHome)
+
+
+// create the varible
+let myFavColor;
+// asign the value
+myFavColor = 'Red'
+
+
+
+
+// Games 
+
+const noobWeapon = 20
+let playerHealth = 100
+
